ff2758b05a9b6ce8ca15051e6f1343d32e69ccfb
superXD <maxzatoplane@gmail.com>
2024-09-02 19:06:11 +0700

    ver_1.3

---
+++ frontend/Blog/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Home />}></Route>
+
+
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
---
+++ frontend/Blog/src/pages/Layout.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom';
+
+const Layout = () => {
+
+    const menu = [
+        { text: 'Nature', path: '/' },
+        { text: 'Travel', path: '/' },
+        { text: 'Technology', path: '/' },
+        { text: 'Politics', path: '/' },
+
+    ]
+    return (
+        <div>
+            {/*<Header></Header>*/}
+            <div className="border-b">
+                <div className="container px-5 py-5 flex justify-between">
+
+                    <span className='font-extrabold text-2xl'>BLOGGER</span>
+                    <div className='flex'>
+                        <ul className='flex'>
+                            {
+                                menu.map(x => {
+                                    return <li><Link className='p-2 items-center justify-center flex'><span>{x.text}</span></Link></li>
+
+                                })
+                            }
+
+                        </ul>
+                        <button className='bg-slate-500 text-white px-2 py-1 rounded'>
+                            <Link>+ New post</Link>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {/*<Body></Body>*/}
+            <div className='flex'>
+                <Outlet> </Outlet>
+            </div>
+            {/*<Footer></Footer>*/}
+            <div className="flex bg-slate-800">
+                <div className="flex mx-auto px-20 py-20 items-center justify-center">
+                    <h3 className='text-gray-400'>BLOGGER</h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Layout;
